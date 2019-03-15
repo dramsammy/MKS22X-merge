a8c3778a06ce0bac7e858c999b83e48d671c47fa
dramsammy <dramsammy@stuy.edu>
2019-03-15 10:51:46 -0400

    added temporary values

--- Merge.java
+++ Merge.java
@@ -1,8 +1,15 @@
 public class Merge{
+  public static int[] temp1;
+  public static int[] temp2;
   public static void mergeSort(int[] data){
     return mergeSort(data, 1, data.length);
   }
   public static void mergeSort(int[] data, int lo, int hi){
+    if (lo >= hi){
+      return;
+    }
 
+    mergeSort()
   }
+  public static void split()
 }
