49ac682b98f15a0510b8a287a05db6ae5d17dcf7
dramsammy <dramsammy@stuy.edu>
2019-03-15 10:47:16 -0400

    finished merge sort

--- Merge.java
+++ Merge.java
@@ -1,3 +1,8 @@
 public class Merge{
-  
+  public static void mergeSort(int[] data){
+    return mergeSort(data, 1, data.length);
+  }
+  public static void mergeSort(int[] data, int lo, int hi){
+
+  }
 }
